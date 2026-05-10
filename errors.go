0ab8d93f1efe7fb908760d75e5280ea078b10eae
Exanubes <blog@exanubes.com>
2026-05-10 20:57:10 +0200

    Add sentinel errors to public API

---
+++ errors.go
@@ -0,0 +1,13 @@
+package appsync
+
+import "github.com/exanubes/appsync/internal/app"
+
+var (
+	ErrEmptyUrl              = app.ErrEmptyUrl
+	ErrHandshakeTimeout      = app.ErrHandshakeTimeout
+	ErrDuplicateMessage      = app.ErrDuplicateMessage
+	ErrSubscriptionInboxFull = app.ErrSubscriptionInboxFull
+	ErrSubscriptionClosed    = app.ErrSubscriptionClosed
+	ErrSubscriptionNotFound  = app.ErrSubscriptionNotFound
+	ErrHeartbeatTimeout      = app.ErrHeartbeatTimeout
+)
